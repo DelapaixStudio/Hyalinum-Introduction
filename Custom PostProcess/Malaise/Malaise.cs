32a2d52f26b6727af7da301803b20eea0dc6dae2
DelapaixStudio <76694370+DelapaixStudio@users.noreply.github.com>
2021-10-10 18:45:32 +0200

    Add files via upload

---
+++ Custom PostProcess/Malaise/Malaise.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[Serializable]
+[PostProcess(typeof(MalaiseRenderer), PostProcessEvent.AfterStack, "Hidden/Cutsom/Malaise")]
+public sealed class Malaise : PostProcessEffectSettings
+{
+    [Range(0f, 1f), Tooltip("Effet de zone de mort")]
+    public FloatParameter blend = new FloatParameter { value = 0.5f };
+    [Range(0f, 1f), Tooltip("Distortion Time")]
+    public FloatParameter dist = new FloatParameter { value = 0.1f };
+    [Tooltip("Texture Overlay")]
+    public TextureParameter texture = new TextureParameter { value = null };
+}
+
+public sealed class MalaiseRenderer : PostProcessEffectRenderer<Malaise>
+{
+    public override void Render(PostProcessRenderContext context)
+    {
+        var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/Malaise"));
+
+        var imageTexture = settings.texture.value == null    // Si il n'y a pas texture 
+            ? RuntimeUtilities.transparentTexture            // Ajout d'une texture transparente
+            : settings.texture.value;                        // Mais si la premiere ligne = false alors on prend la texture depuis la class Malaise
+
+        sheet.properties.SetTexture("_TextureNoise", imageTexture);
+
+        sheet.properties.SetFloat("_Distortion", settings.dist);
+        sheet.properties.SetFloat("_Blend", settings.blend);
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+    }
+}
---
+++ Custom PostProcess/Malaise/Malaise.shader
@@ -0,0 +1,46 @@
+﻿Shader "Hidden/Custom/Malaise"
+{
+    HLSLINCLUDE
+
+        #include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
+
+        TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+        float _Blend;
+        float _Distortion;
+		sampler2D _TextureNoise;
+
+        float4 Frag(VaryingsDefault i) : SV_Target
+        {
+		  float2 uv = i.texcoord;
+		  float2 offset = float2(0, tex2D(_TextureNoise, float2( _Time[_Distortion] , uv.y)).r);
+		  uv += offset / 2;
+
+		  float4 color = (SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv) );
+		  
+
+
+
+
+            float luminance = dot(color.rgb, float3(0.2126729, 0.7151522, 0.0721750));
+            color.rgb = lerp(color.rgb, luminance.xxx, _Blend.xxx);
+
+            return color;
+        }
+
+    ENDHLSL
+
+    SubShader
+    {
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            HLSLPROGRAM
+
+                #pragma vertex VertDefault
+                #pragma fragment Frag
+
+            ENDHLSL
+        }
+    }
+}
